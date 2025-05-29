6ff3cd51973f0e3d2bd36450b7e1d8e675801fdd
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-05-29 13:06:30 -0700

    Added security, added frontend and added refresh token'

--- frontend/Fronted/src/App.jsx
+++ frontend/Fronted/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { InputAdornment,TextField } from '@mui/material'
+import EmployeeForm from './EmployeeForm'
+import Home from './Home'
 function App() {
   return (
     <div>
      <BrowserRouter>
   <Routes>
     <Route path='/' element={<Home />} />
+    <Route path='/employee' element={<EmployeeForm/>}/>
   </Routes>
 </BrowserRouter>
 
--- frontend/Fronted/src/Home.jsx
+++ frontend/Fronted/src/Home.jsx
@@ -2,20 +2,57 @@ import React from 'react'
 import { Box, Typography,Button } from '@mui/material';
 import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
 import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+import { Link } from 'react-router-dom';
 
 
 function Home() {
     return (
-        <Box  display="flex" justifyContent='space-between'>
-            <Box>
-<PeopleOutlineIcon />
+      <Box
+      position='fixed'
+  display="flex"
+  justifyContent="space-between"
+  alignItems="center"
+  width="100vw"
+  
+  px={4}
+>
+  {/* Left Section: Icon + Login Button */}
+  <Box
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    sx={{ml:10}}
+  >
+    <PeopleOutlineIcon sx={{ fontSize: 250 }} />
+    <Button
+      component={Link}
+      to="/employee"
+      variant="contained"
+      sx={{ width: "200px", mt: 2 }} // mt: margin-top
+    >
+      Employee
+    </Button>
+  </Box>
 
-            </Box>
-            <Box>
+  {/* Right Section: Sign In */}
+  <Box
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    sx={{mr:10}}
+  >
+    <GroupsOutlinedIcon sx={{ fontSize: 250 }} />
+   <Button
+      component={Link}
+      to="/clients"
+      variant="contained"
+      sx={{ width: "200px", mt: 2 }} // mt: margin-top
+    >
+      Clients
+    </Button>
+  </Box>
+</Box>
 
-            </Box>
-
-        </Box>
     )
 }
 
